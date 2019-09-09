f8e411b7cb4bc929446084a97c027b031a3de0ca
shinmura0 <34497776+shinmura0@users.noreply.github.com>
2019-09-09 16:54:40 +0900

    update

--- libs/util.py
+++ libs/util.py
@@ -14,11 +14,11 @@ def random_mask(height, width, channels=3):
         raise Exception("Width and Height of mask must be at least 64!")
     
     # Draw random lines
-    for _ in range(randint(1, 20)):
-        x1, x2 = randint(1, width), randint(1, width)
-        y1, y2 = randint(1, height), randint(1, height)
-        thickness = randint(3, size)
-        cv2.line(img,(x1,y1),(x2,y2),(1,1,1),thickness)
+    #for _ in range(randint(1, 20)):
+    #    x1, x2 = randint(1, width), randint(1, width)
+    #    y1, y2 = randint(1, height), randint(1, height)
+    #    thickness = randint(3, size)
+    #    cv2.line(img,(x1,y1),(x2,y2),(1,1,1),thickness)
         
     # Draw random circles
     #for _ in range(randint(1, 20)):
@@ -26,17 +26,17 @@ def random_mask(height, width, channels=3):
     #    radius = randint(3, size)
     #    cv2.circle(img,(x1,y1),radius,(1,1,1), -1)
         
-    for _ in range(randint(1, 20)):
+    for _ in range(randint(1, 40)):
         x1, y1 = randint(1, width), randint(1, height)
-        length = randint(3, 2*size)
+        length = randint(3, 3*size)
         cv2.rectangle(img, (x1, y1), (x1+length, y1+length), (1, 1, 1), thickness=-1)
         
     # Draw random ellipses
-    for _ in range(randint(1, 20)):
-        x1, y1 = randint(1, width), randint(1, height)
-        s1, s2 = randint(1, width), randint(1, height)
-        a1, a2, a3 = randint(3, 180), randint(3, 180), randint(3, 180)
-        thickness = randint(3, size)
-        cv2.ellipse(img, (x1,y1), (s1,s2), a1, a2, a3,(1,1,1), thickness)
+    #for _ in range(randint(1, 20)):
+    #    x1, y1 = randint(1, width), randint(1, height)
+    #    s1, s2 = randint(1, width), randint(1, height)
+    #    a1, a2, a3 = randint(3, 180), randint(3, 180), randint(3, 180)
+    #    thickness = randint(3, size)
+    #    cv2.ellipse(img, (x1,y1), (s1,s2), a1, a2, a3,(1,1,1), thickness)
     
     return 1-img
