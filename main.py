c66397f8d39eea30d2f893d5efe99304847ea0a3
shinmura0 <34497776+shinmura0@users.noreply.github.com>
2019-11-16 20:04:38 +0900

    cvtColor add

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import cv2
 import time
 import os
 import numpy as np
-from libs.pconv_model import PConvUnet
+#from libs.pconv_model import PConvUnet
+from libs.pconv_layer import PConv2D
 import keras
 from keras.models import model_from_json
 from keras import backend as K
 
-m_input_size, m_input_size = 224, 224
+m_input_size, m_input_size = 256, 256
 
 print("Model loading...")
-model = PConvUnet(img_rows=256, img_cols=256)
-model.load("model/model.h5")
-    
+model = keras.models.load_model('model/model.h5', compile=False, custom_objects={'PConv2D': PConv2D})
+
 def main():
-    camera_width =  300
-    camera_height = 200
+    camera_width =  352
+    camera_height = 288
     fps = ""
     elapsedTime = 0
 
@@ -32,9 +32,11 @@ def main():
         ret, image = cap.read()
         if not ret:
             break
+
+        image = image[16:272, 48:304]
             
         # Mask
-        img = np.zeros((256, 256, 3), np.uint8)
+        img = np.zeros(image.shape, np.uint8)
         cv2.rectangle(img, (70, 70), (130, 130), (1, 1, 1), thickness=-1)
         mask = 1-img
 
@@ -44,11 +46,15 @@ def main():
         img[mask==0] = 1
         predict_img = model.predict([np.expand_dims(img, axis=0), np.expand_dims(mask, axis=0)])
 
-        cv2.imshow("Result", predict_img)
+        output = predict_img.reshape(image.shape)
+        output = cv2.cvtColor(output, cv2.COLOR_RGB2BGR)
+        cv2.putText(output, fps, (camera_width-100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
+        cv2.imshow("Result", output)
             
         # FPS
         elapsedTime = time.time() - t1
         fps = "{:.0f} FPS".format(1/elapsedTime)
+        print(fps)
 
         # quit
         if cv2.waitKey(1)&0xFF == ord('q'):
